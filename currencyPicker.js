18b6384cc74cc350e00b24bf60168f9e870267a0
Helena Silkina <helenasilkina@gmail.com>
2014-12-05 13:24:08 +0300

    by default currency has value 0

--- currencyPicker.js
+++ currencyPicker.js
@@ -5,7 +5,7 @@ app.directive('currencyPicker', function () {
 			currencyModel: '=',
 			currencies: '='
 		},
-		template: '<fieldset class="travel-currency-block"><input type="text" ng-model="currencyModel.Value" class="travel-form-element" ng-change="currencyValidate();" /><select class="travel-form-element" ng-model="currencyModel.Currency"><option ng-init="currencyModel.Currency=currencies[1].Title" ng-selected="currencyModel.Currency==currency.Title" ng-repeat="currency in currencies" value="{{currency.Title}}">{{currency.Title}}</option></select></fieldset>',
+		template: '<fieldset class="travel-currency-block"><input type="text" ng-model="currencyModel.Value" ng-init="currencyModel.Value=0" class="travel-form-element" ng-change="currencyValidate();" /><select class="travel-form-element" ng-model="currencyModel.Currency"><option ng-init="currencyModel.Currency=currencies[1].Title" ng-selected="currencyModel.Currency==currency.Title" ng-repeat="currency in currencies" value="{{currency.Title}}">{{currency.Title}}</option></select></fieldset>',
         replace: true,
         controller: function ($scope) {	
     			$scope.currencyValidate = function() {
