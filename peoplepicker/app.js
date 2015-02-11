ef9bcafcd56ddfd61cc264b83dfefc2d0fe80806
a_silkina <a_silkina@wargaming.net>
2015-02-11 12:42:18 +0300

    add mock for http request

--- peoplepicker/app.js
+++ peoplepicker/app.js
@@ -1,5 +1,20 @@
-angular.module('ui.bootstrap.demo', ['ui.bootstrap']);
-angular.module('ui.bootstrap.demo', ['peoplepickerModule']).controller('TypeaheadCtrl', function($scope, $http) {
-	$scope.peopleSelected = [];
-	$scope.peopleQuery= 'http://maps.googleapis.com/maps/api/geocode/json';
- }); 
+angular.module('ui.bootstrap.demo', ['ui.bootstrap', 'peoplepickerModule', 'ngMockE2E'])
+    .controller('TypeaheadCtrl', function ($scope, $http) {
+        $scope.peopleSelected = [];
+        $scope.peopleQuery = '/people';
+    })
+.run(function ($httpBackend) {
+    $httpBackend.whenGET(/\/people/)
+        .respond(function (method, url, data) {
+            var name = decodeURIComponent(url.slice(url.indexOf('=')+1, url.length));
+            var response = [];
+            for (var i = 10; i--;) {
+                var rand = parseInt(Math.random() * 100)
+                response.push({
+                    Name: name + rand,
+                    Id: 'id_' + name + rand
+                });
+            }
+            return response;
+        })
+});
